54157b087eecc553826eeb90920118183fa588b0
Akuma <ragna.blood82@gmail.com>
2023-10-17 20:49:55 +0300

    Complete

--- Home_W_9.py
+++ Home_W_9.py
@@ -1,10 +1,6 @@
 N = int(input())
 numbers = list(map(int, input().split()))
-
-unique_numbers = set(numbers)
-count = len(unique_numbers)
-
-print(count)
+print(len(set(numbers)))
 
                                        # Второе задание
 
